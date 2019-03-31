230b74fe63424a717e045043a33b1dff5ff8aaeb
Jenkins <jaya.maduka@gmail.com>
2019-03-31 13:08:46 +0800

    Basic security

--- src/main/java/com/mac/rest/webservices/restfulwebservices/versioning/PersonVersioningController.java
+++ src/main/java/com/mac/rest/webservices/restfulwebservices/versioning/PersonVersioningController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class PersonVersioningController {
+	
+	
 	/* ################################################## */
 	// URI versioning
 	/* ################################################## */
